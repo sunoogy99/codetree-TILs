6a2cddfe682d2f18986c990ab1ffd95f245bbaaf
sunoogy99 <76517609+sunoogy99@users.noreply.github.com>
2024-02-27 16:29:22 +0900

    Title: 정수 사각형 최대 합, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240227/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
+++ 240227/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -9,13 +9,13 @@ int max(int a, int b) {
 	return a > b ? a : b;
 }
 
-long long dp(int r, int c) {
-	if (r > 0 && c > 0) {
-		ans[r][c] = max(dp(r - 1, c), dp(r, c - 1)) + arr[r][c];
-		return ans[r][c];
-	}
-	else return 0;
-}
+//long long dp(int r, int c) {
+//	if (r > 0 && c > 0) {
+//		ans[r][c] = max(dp(r - 1, c), dp(r, c - 1)) + arr[r][c];
+//		return ans[r][c];
+//	}
+//	else return 0;
+//}
 
 int main() {
 	cin >> n;
@@ -23,9 +23,11 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			cin >> arr[i][j];
+
+			ans[i][j] = arr[i][j] + max(ans[i - 1][j], ans[i][j - 1]);
 		}
 	}
 
-	cout << dp(n, n);
+	cout << ans[n][n];
 	return 0;
 }
