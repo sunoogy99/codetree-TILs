5bdc670f54c94ccf6cd096ed14198f34673d5fa8
sunoogy99 <76517609+sunoogy99@users.noreply.github.com>
2024-03-18 20:52:24 +0900

    Title: 구간 중 최대 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n, k, part_sum;
+int arr[101];
+int max_val = INT_MIN;
+
+int main() {
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+
+		if (i > 2) {
+			part_sum += arr[i];
+			part_sum -= arr[i - 3];
+
+			if (part_sum > max_val) {
+				max_val = part_sum;
+			}
+		}
+		else {
+			part_sum += arr[i];
+		}
+	}
+
+	cout << max_val << endl;
+	return 0;
+}
