c6c636a8606b20fd68c745da9ffe19b2e43ca338
sunoogy99 <76517609+sunoogy99@users.noreply.github.com>
2024-04-16 00:00:30 +0900

    Title: 1, 2, 5 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240416/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,31 @@
+// https://www.codetree.ai/missions/2/problems/1-2-5-plus/description
+// 1,2,5 더하기
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int n;
+int num[3] = { 1,2,5 };
+int dp[1001];
+
+int main() {
+	cin >> n;
+
+	// 1,2,5는 자기 숫자에 의해 만들어지는 경우 1가지는 무조건 존재함
+	for (int i = 0; i < 3; i++)
+		dp[num[i]] = 1;
+
+	// 더해지는 순서가 다르면 다른 조합임
+	// 이전까지의 합을 만드는 경우의 수들을 dp[i]에 계속 더해주면 된다.
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (i >= num[j]) {
+				dp[i] += dp[i - num[j]];
+			}
+		}
+	}
+
+	cout << dp[n];
+	return 0;
+}
