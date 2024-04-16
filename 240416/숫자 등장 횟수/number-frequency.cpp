88f172bb65281b8b4fe46ed3d10c22e08d97fd6b
sunoogy99 <76517609+sunoogy99@users.noreply.github.com>
2024-04-16 23:42:27 +0900

    Title: 숫자 등장 횟수, Time: 160ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 240416/숫자 등장 횟수/number-frequency.cpp
@@ -0,0 +1,30 @@
+// https://www.codetree.ai/missions/8/problems/number-frequency/description
+// 숫자 등장 횟수
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int n, m;
+int arr[100001];
+unordered_map<int, int> num_to_index;
+
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		num_to_index[arr[i]] = i + 1;
+	}
+
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		if (num_to_index.find(num) != num_to_index.end()) {
+			cout << num_to_index[num] << ' ';
+		}
+		else
+			cout << 0 << ' ';
+	}
+
+	return 0;
+}
