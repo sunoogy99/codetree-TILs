6d189af39e861327f9ff9d8ca3d8eeacf336bfb3
sunoogy99 <76517609+sunoogy99@users.noreply.github.com>
2024-06-28 17:12:30 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 240ms, Memory: 16MB, Status: Passed - CodeTree

--- 240628/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.js
+++ 240628/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.js
@@ -11,9 +11,6 @@ const dx = [0, 1];
 const dy = [1, 0];
 
 const solution = (data, visited) => {
-    // 뱀을 피해서 갈 수 있는 경로 여부를 저장하는 변수
-    let isPathExist = false;
-
     function inRange(x, y) {
         return x >= 0 && x < N && y >= 0 && y < M;
     }
@@ -33,12 +30,7 @@ const solution = (data, visited) => {
     }
 
     const dfs = (x, y) => {
-        // 우측 하단에 도달하고 뱀이 존재하지 않는 경우,
-        // 또는 우측 하단에 도달하여 경로가 존재함을 알고 있는 경우
-        if (isPathExist || (x === N - 1 && y === M - 1 && data[x][y])) {
-            isPathExist = true;
-            return;
-        }
+        visited[x][y] = true;
 
         for (let i = 0; i < 2; i++) {
             let newX = x + dx[i];
@@ -46,15 +38,13 @@ const solution = (data, visited) => {
 
             if (canGo(newX, newY)) {
                 dfs(newX, newY);
-                visited[newX, newY] = true;
             }
         }
     }
 
-    visited[0][0] = true;
     dfs(0, 0);
 
-    if (isPathExist) {
+    if (visited[N - 1][M - 1]) {
         console.log(1);
     }
     else {
