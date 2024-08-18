17b72e42d079e77e7b548494ab7ec060d2afa267
sunoogy <76517609+sunoogy99@users.noreply.github.com>
2024-08-18 23:04:28 +0900

    Title: 트리 순회, Time: 1086ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240818/트리 순회/the-tree-traversal.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+char tree[27];
+char aIdx[27];
+
+// 전위 순회
+void preOrder(int idx) {
+	cout << tree[idx];
+	if (tree[2 * idx] != '.') preOrder(2 * idx);
+	if (tree[2 * idx + 1] != '.') preOrder(2 * idx + 1);
+}
+
+// 중위 순회
+void inOrder(int idx) {
+	if (tree[2 * idx] != '.') inOrder(2 * idx);
+	cout << tree[idx];
+	if (tree[2 * idx + 1] != '.') inOrder(2 * idx + 1);
+}
+
+// 후위 순회
+void postOrder(int idx) {
+	if (tree[2 * idx] != '.') postOrder(2 * idx);
+	if (tree[2 * idx + 1] != '.') postOrder(2 * idx + 1);
+	cout << tree[idx];
+}
+
+int main() {
+	cin >> n;
+
+	char node, left, right;
+	aIdx[0] = 1; // 'A' 인덱스를 1로 설정
+
+	// 이진 탐색 트리 생성하기
+	for (int i = 1; i <= n; i++) {
+		cin >> node >> left >> right;
+		
+		int cur = aIdx[node - 'A'];
+
+		tree[cur] = node;
+
+		int lChild = 2 * cur;
+		if (left != '.') aIdx[left - 'A'] = lChild;
+		tree[lChild] = left;
+
+		int rChild = 2 * cur + 1;
+		if (right != '.') aIdx[right - 'A'] = rChild;
+		tree[rChild] = right;
+	}
+
+	preOrder(1);
+	cout << '\n';
+
+	inOrder(1);
+	cout << '\n';
+
+	postOrder(1);
+	cout << '\n';
+
+	return 0;
+}
