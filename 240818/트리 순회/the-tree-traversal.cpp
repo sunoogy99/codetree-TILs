0aaf33934d6b7a43be3e947d3468138820a41c09
sunoogy <76517609+sunoogy99@users.noreply.github.com>
2024-08-18 23:22:00 +0900

    Title: 트리 순회, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240818/트리 순회/the-tree-traversal.cpp
+++ 240818/트리 순회/the-tree-traversal.cpp
@@ -1,66 +1,49 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 
 int n;
-char tree[300];
-char aIdx[300];
+vector<vector<int>> tree;
 
-// 전위 순회
-void preOrder(int idx) {
-	cout << tree[idx];
-	if (tree[2 * idx] != '.') preOrder(2 * idx);
-	if (tree[2 * idx + 1] != '.') preOrder(2 * idx + 1);
+void preOrder(char x) {
+	cout << x;
+	if (tree[x - 'A'][0] != '.') preOrder(tree[x - 'A'][0]);
+	if (tree[x - 'A'][1] != '.') preOrder(tree[x - 'A'][1]);
 }
 
-// 중위 순회
-void inOrder(int idx) {
-	if (tree[2 * idx] != '.') inOrder(2 * idx);
-	cout << tree[idx];
-	if (tree[2 * idx + 1] != '.') inOrder(2 * idx + 1);
+void inOrder(char x) {
+	if (tree[x - 'A'][0] != '.') inOrder(tree[x - 'A'][0]);
+	cout << x;
+	if (tree[x - 'A'][1] != '.') inOrder(tree[x - 'A'][1]);
 }
 
-// 후위 순회
-void postOrder(int idx) {
-	if (tree[2 * idx] != '.') postOrder(2 * idx);
-	if (tree[2 * idx + 1] != '.') postOrder(2 * idx + 1);
-	cout << tree[idx];
+void postOrder(char x) {
+	if (tree[x - 'A'][0] != '.') postOrder(tree[x - 'A'][0]);
+	if (tree[x - 'A'][1] != '.') postOrder(tree[x - 'A'][1]);
+	cout << x;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	tree.resize(26);
+
 	cin >> n;
 
-	char node, left, right;
-	aIdx[0] = 1; // 'A' 인덱스를 1로 설정
+	// 트리 생성
+	char cur, left, right;
+	for (int i = 0; i < n; i++) {
+		cin >> cur >> left >> right;
 
-	// 이진 탐색 트리 생성하기
-	for (int i = 1; i <= n; i++) {
-		cin >> node >> left >> right;
-		
-		int cur = aIdx[node - 'A'];
-
-		tree[cur] = node;
-
-		int lChild = 2 * cur;
-		if (left != '.') aIdx[left - 'A'] = lChild;
-		tree[lChild] = left;
-
-		int rChild = 2 * cur + 1;
-		if (right != '.') aIdx[right - 'A'] = rChild;
-		tree[rChild] = right;
+		tree[cur - 'A'].push_back(left);
+		tree[cur - 'A'].push_back(right);
 	}
 
-	preOrder(1);
+	preOrder('A');
 	cout << '\n';
 
-	inOrder(1);
+	inOrder('A');
 	cout << '\n';
 
-	postOrder(1);
-	cout << '\n';
-
+	postOrder('A');
+	
 	return 0;
 }
